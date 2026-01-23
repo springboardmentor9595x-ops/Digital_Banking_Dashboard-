ecf055fbd122e60e91e9d43912090c795de42fa5
AnukalpTejaswi <anukalptejaswi848125@gmail.com>
2026-01-23 13:43:40 +0530

    Frontend: Updated notification library

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,24 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { ToastContainer } from 'react-toastify';   // {/* Notification library */}
-import 'react-toastify/dist/ReactToastify.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-    
-    {/* Notification container */}
-    <ToastContainer               
-      position="top-right"
-      autoClose={3000}
-      hideProgressBar={false}
-      closeOnClick
-      pauseOnHover
-      draggable
-    />
-                    
-  </React.StrictMode>
+   
 );
